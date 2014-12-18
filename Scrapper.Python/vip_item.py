480d43ca58b10ced2f892afdd059fe50a72f16ff
Geunho Khim <gkhim@ebay.com>
2014-12-18 23:35:15 +0900

    no message

--- Scrapper.Python/vip_item.py
+++ Scrapper.Python/vip_item.py
@@ -1,6 +1,7 @@
 from scrapy.item import Item, Field
 
 class ViewItemPage(Item):
+  itemno = Field(default='')
   title = Field(default='')
   imageUrl = Field(default='')
   price = Field(default=0)
